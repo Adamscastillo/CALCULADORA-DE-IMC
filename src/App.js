6e90441fbfc66d43777dbf8ec71308c6c724fe86
Adams Castillo <102859028+Adamscastillo@users.noreply.github.com>
2022-09-03 11:59:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ function App() {
     
 
     //se o imc estiver entre 18,5 ate 24,9 normal | entre 25,0 e 29,9 sobre peso | acima de 30,0 obesidade | maior que 40,0 obsidade grave
-    if (imc < 18.6) {
+    if (imc < 18.5) {
       setMensagem("Está abaixo do Peso!!! seu IMC : " + imc.toFixed(2));
-    } else if (imc >=18.6 && imc <24.9){
+    } else if (imc >=18.5 && imc <24.9){
       setMensagem("Está com Peso ideal !!! seu IMC : " + imc.toFixed(2));
-    }else if (imc >=24.9 && imc <29.9){
+    }else if (imc >=25.0 && imc <29.9){
       setMensagem("Está com sobre Peso, Atenção!!! seu IMC : " + imc.toFixed(2));
-    }else if (imc >=29.9 && imc <30.0){
+    }else if (imc >=30.0 && imc <39.9){
       setMensagem("Está acima do Peso, Obesidade é coisa seria!!! seu IMC : " + imc.toFixed(2));
-    }else if(imc>30.0 ){
+    }else if(imc>40.0 ){
       setMensagem("Obesidade grave, Alerta!!! seu IMC : " + imc.toFixed(2));
     };
 
